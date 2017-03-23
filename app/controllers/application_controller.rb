accd9b3760e47dcb3bb0098c928edfa5c2a82ad8
manik <manik.impinge@gmail.com>
2017-03-23 16:17:09 +0530

    public_activity gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  include PublicActivity::StoreController 
 
   before_action :authenticate_user!
 
   def contacts
+  	@activities = PublicActivity::Activity.all
   end
   def agenda
   end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,7 @@
 class Event < ActiveRecord::Base
   belongs_to :user
+
+  include PublicActivity::Model
+  # tracked
+  tracked owner: Proc.new{ |controller, model| controller.current_user }
 end
